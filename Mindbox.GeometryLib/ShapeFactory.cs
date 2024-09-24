11b868af3c96905a88aea591ce8a8093ab001f02
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 23:33:21 +0500

    FEAT: add documentation to shape factory

--- Mindbox.GeometryLib/ShapeFactory.cs
+++ Mindbox.GeometryLib/ShapeFactory.cs
@@ -1,13 +1,32 @@
 ﻿namespace Mindbox.GeometryLib
 {
+    /// <summary>
+    /// Universal <seealso cref="IShape"/> factory, based on reflection
+    /// </summary>
     public static class ShapeFactory
     {
+        /// <summary>
+        /// Creates instance of shape, that implements <seealso cref="IShape"/> contract
+        /// </summary>
+        /// <typeparam name="T">shape type</typeparam>
+        /// <param name="args">ctor arguments</param>
+        /// <returns>
+        /// instance of shape
+        /// </returns>
         public static T CreateShape<T>(params object[] args) where T : IShape
         {
             Type shape = typeof(T);
             return (T)Activator.CreateInstance(shape, args)!;
         }
 
+        /// <summary>
+        /// Creates list of shapes, that implements <seealso cref="IShape"> contract
+        /// </summary>
+        /// <typeparam name="T">shape type</typeparam>
+        /// <param name="shapesInfo">ctor arhuments</param>
+        /// <returns>
+        /// list of shapes
+        /// </returns>
         public static IEnumerable<T> CreateShapes<T>(IEnumerable<(T, object[] args)> shapesInfo) where T : IShape
         {
             var shapes = new List<T>();
