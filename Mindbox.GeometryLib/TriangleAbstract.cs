62d64041a553e080035a06f1157588925b460e03
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 20:25:26 +0500

    REFACTOR: make triangle class abstract

--- Mindbox.GeometryLib/TriangleAbstract.cs
+++ Mindbox.GeometryLib/TriangleAbstract.cs
@@ -1,19 +1,27 @@
 ﻿namespace Mindbox.GeometryLib
 {
-    public class Triangle : IShape
+    public abstract class TriangleAbstract : IShape
     {
         public double SideA { get; protected set; }
         public double SideB { get; protected set; }
         public double SideC { get; protected set; }
 
-        public Triangle(double sideLength)
+        protected TriangleAbstract(double sideLength)
         {
+            if (!isSidesValid(sideLength))
+            {
+                throw new ArgumentOutOfRangeException(nameof(sideLength), "Side has to be > 0");
+            }
             this.SideA = sideLength;
             this.SideB = sideLength;
             this.SideC = sideLength;
         }
-        public Triangle(double sideA, double sideB, double sideC)
+        protected TriangleAbstract(double sideA, double sideB, double sideC)
         {
+            if (!isSidesValid(sideA, sideB, sideC))
+            {
+                throw new ArgumentOutOfRangeException("Side has to be > 0");
+            }
             this.SideA = sideA;
             this.SideB = sideB;
             this.SideC = sideC;
@@ -27,6 +35,15 @@
             double a = Math.Pow(sides.ElementAt(1), 2);
             double b = Math.Pow(sides.ElementAt(0), 2);
             return Math.Abs(c - (b + a)) < 0.0001;
+        }
+
+        protected bool isSidesValid(params double[] sides)
+        {
+            foreach(var side in sides)
+            {
+                if(side < 0) return false;
+            }
+            return true;
         }
 
         #region CONTRACTS_IMPLEMENTATION
