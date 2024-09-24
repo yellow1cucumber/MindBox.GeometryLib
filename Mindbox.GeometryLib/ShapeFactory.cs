acb911147edb435e8a2fe81e11c2a6c388345b4b
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 23:21:07 +0500

    FEAT: implement shape factory

---
+++ Mindbox.GeometryLib/ShapeFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace Mindbox.GeometryLib
+{
+    public static class ShapeFactory
+    {
+        public static T CreateShape<T>(params object[] args) where T : IShape
+        {
+            Type shape = typeof(T);
+            return (T)Activator.CreateInstance(shape, args)!;
+        }
+
+        public static IEnumerable<T> CreateShapes<T>(IEnumerable<(T, object[] args)> shapesInfo) where T : IShape
+        {
+            var shapes = new List<T>();
+            foreach (var (type, args) in shapesInfo)
+            {
+                Type shapeType = typeof(T);
+                var shape = (T)Activator.CreateInstance(shapeType, args)!;
+                shapes.Add(shape);
+            }
+            return shapes;
+        }
+    }
+}
