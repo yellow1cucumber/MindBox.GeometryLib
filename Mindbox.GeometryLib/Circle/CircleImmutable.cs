f2b19677277591ed90c2ee121cd0dfb5712b931d
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 23:41:46 +0500

    FEAT: add docs to CircleImmutable

--- Mindbox.GeometryLib/Circle/CircleImmutable.cs
+++ Mindbox.GeometryLib/Circle/CircleImmutable.cs
@@ -1,5 +1,11 @@
 ﻿namespace Mindbox.GeometryLib.Circle
 {
+    /// <summary>
+    /// Common circle. Cant be modified.
+    /// <para>See also:</para>
+    /// <para><seealso cref="CircleAbstract"/> - base class</para>
+    /// <para><seealso cref="CircleMutable"/> - for modifieable circles</para>
+    /// </summary>
     public sealed class CircleImmutable : CircleAbstract
     {
         public override double Radius { get; protected set; }
