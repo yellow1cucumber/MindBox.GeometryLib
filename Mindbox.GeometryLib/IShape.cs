d5f7b1ac0d5dca5b39b9d9f9ddd09f590a0b3fcb
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 23:35:07 +0500

    FEAT: add docs to IShape

--- Mindbox.GeometryLib/IShape.cs
+++ Mindbox.GeometryLib/IShape.cs
@@ -1,7 +1,16 @@
 ﻿namespace Mindbox.GeometryLib
 {
+    /// <summary>
+    /// Contract, that defines common behavior of shapes
+    /// </summary>
     public interface IShape
     {
+        /// <summary>
+        /// Method, that returns area of geometry shape
+        /// </summary>
+        /// <returns>
+        /// area of shape
+        /// </returns>
         public double GetArea();
     }
 }
