64326d1487d6b189072cc12249db0e8ff383b763
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-25 00:23:14 +0500

    FEAT: add sql request

---
+++ Mindbox.GeometryLib/SQL_task.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+    Products.ProductName, 
+    Categories.CategoryName
+FROM 
+    Products
+LEFT JOIN 
+    ProductCategories ON Products.ProductID = ProductCategories.ProductID
+LEFT JOIN 
+    Categories ON ProductCategories.CategoryID = Categories.CategoryID;
