9996e4f481fc8d79e4e8c338d5ab941071425e8d
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 19:41:20 +0500

    FEAT: implement immutable circle class

---
+++ Mindbox.GeometryLib/CircleImmutable.cs
@@ -0,0 +1,10 @@
+﻿namespace Mindbox.GeometryLib
+{
+    public class CircleImmutable : CircleAbstract
+    {
+        public override double Radius { get; protected set; }
+
+        public CircleImmutable(double radius)
+            : base(radius) { }
+    }
+}
