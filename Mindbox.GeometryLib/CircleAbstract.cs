3ce0d63ab82739772aab59043f069211192873cf
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 19:36:13 +0500

    FEAT: implement AbstractCircle class

---
+++ Mindbox.GeometryLib/CircleAbstract.cs
@@ -0,0 +1,27 @@
+﻿namespace Mindbox.GeometryLib
+{
+    public abstract class CircleAbstract : IShape
+    {
+        public abstract double Radius { get; protected set; }
+
+        protected CircleAbstract(double radius)
+        {
+            if (!isRadiusValid(radius))
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius), "Radius has to be > 0");
+            }
+            this.Radius = radius;
+        }
+
+        private bool isRadiusValid(double radius)
+            => radius > 0;
+
+
+        #region CONTRACT_IMPLEMENTATION
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(this.Radius, 2);
+        }
+        #endregion
+    }
+}
