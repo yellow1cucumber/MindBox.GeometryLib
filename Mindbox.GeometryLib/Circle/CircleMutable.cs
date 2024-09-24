d3731a5a1e29e7e327189919af5ab128590cdfd2
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 21:46:20 +0500

    REFACTOR: change event entry in stack

--- Mindbox.GeometryLib/Circle/CircleMutable.cs
+++ Mindbox.GeometryLib/Circle/CircleMutable.cs
@@ -1,15 +1,20 @@
 ﻿
+
 namespace Mindbox.GeometryLib.Circle
 {
-    public sealed class CircleMutable : CircleAbstract
+    public sealed class CircleMutable : CircleAbstract,
+                                        IMutable
     {
         public override double Radius { get; protected set; }
         public double Area { get; private set; }
+
+        public event Action? onChanges;
 
         public void SetRadius(double radius)
         {
             Radius = radius;
             Area = GetArea();
+            this.onChanges?.Invoke();
         }
 
 
