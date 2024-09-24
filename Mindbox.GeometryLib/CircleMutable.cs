b0bb27a725ff6dac7339287b00a24fcb14721ae9
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-24 19:39:51 +0500

    FEAT: implement MutableCircle class derived from CircleAbstract class

---
+++ Mindbox.GeometryLib/CircleMutable.cs
@@ -0,0 +1,19 @@
+﻿namespace Mindbox.GeometryLib
+{
+    public class CircleMutable : CircleAbstract
+    {
+        public override double Radius { get; protected set; }
+        public double Area { get; private set; }
+
+        public void SetRadius(double radius)
+        {
+            this.Radius = radius;
+            this.Area = base.GetArea();
+        }
+
+
+        public CircleMutable(double radius) 
+            : base(radius)
+            => this.Area = base.GetArea();
+    }
+}
