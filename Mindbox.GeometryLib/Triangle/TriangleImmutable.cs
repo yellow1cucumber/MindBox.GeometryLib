eeeb378518c83d48540e0279c9e8342c2aaa2d5f
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-25 11:48:07 +0500

    FIX: override hash code

--- Mindbox.GeometryLib/Triangle/TriangleImmutable.cs
+++ Mindbox.GeometryLib/Triangle/TriangleImmutable.cs
@@ -1,4 +1,5 @@
-﻿namespace Mindbox.GeometryLib.Triangle
+﻿
+namespace Mindbox.GeometryLib.Triangle
 {
     /// <summary>
     /// Common triangle. Cant be modified
@@ -6,7 +7,7 @@
     /// <para><seealso cref="TriangleAbstract"/> - base class</para>
     /// <para><seealso cref="TriangleMutable"/> - for modifiable triangles</para>
     /// </summary>
-    public sealed class TriangleImmutable : TriangleAbstract
+    public sealed class TriangleImmutable : TriangleAbstract, IEquatable<TriangleImmutable?>
     {
         public override double SideA { get; protected set; }
         public override double SideB { get; protected set; }
@@ -22,5 +23,40 @@
         /// </summary>
         public TriangleImmutable(double sideA, double sideB, double sideC) 
             : base(sideA, sideB, sideC){}
+
+
+        #region CONTRACTS_IMPLEMENTATION
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as TriangleImmutable);
+        }
+
+        public bool Equals(TriangleImmutable? other)
+        {
+            return other is not null &&
+                   base.Equals(other) &&
+                   SideA == other.SideA &&
+                   SideB == other.SideB &&
+                   SideC == other.SideC &&
+                   SideA == other.SideA &&
+                   SideB == other.SideB &&
+                   SideC == other.SideC;
+        }
+        #endregion
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), SideA, SideB, SideC, SideA, SideB, SideC);
+        }
+
+        public static bool operator ==(TriangleImmutable? left, TriangleImmutable? right)
+        {
+            return EqualityComparer<TriangleImmutable>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(TriangleImmutable? left, TriangleImmutable? right)
+        {
+            return !(left == right);
+        }
     }
 }
