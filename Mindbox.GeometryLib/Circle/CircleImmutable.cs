a9dff2517570e6a2de9784617004c0c6fb49e6c6
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-09-25 11:52:39 +0500

    FIX: hashcode override

--- Mindbox.GeometryLib/Circle/CircleImmutable.cs
+++ Mindbox.GeometryLib/Circle/CircleImmutable.cs
@@ -1,4 +1,5 @@
-﻿namespace Mindbox.GeometryLib.Circle
+﻿
+namespace Mindbox.GeometryLib.Circle
 {
     /// <summary>
     /// Common circle. Cant be modified.
@@ -6,11 +7,40 @@
     /// <para><seealso cref="CircleAbstract"/> - base class</para>
     /// <para><seealso cref="CircleMutable"/> - for modifieable circles</para>
     /// </summary>
-    public sealed class CircleImmutable : CircleAbstract
+    public sealed class CircleImmutable : CircleAbstract, IEquatable<CircleImmutable?>
     {
         public override double Radius { get; protected set; }
 
         public CircleImmutable(double radius)
             : base(radius) { }
+
+        #region CONTRACTS_IMPLEMENTATION
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as CircleImmutable);
+        }
+
+        public bool Equals(CircleImmutable? other)
+        {
+            return other is not null &&
+                   Radius == other.Radius &&
+                   Radius == other.Radius;
+        }
+
+        public static bool operator ==(CircleImmutable? left, CircleImmutable? right)
+        {
+            return EqualityComparer<CircleImmutable>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(CircleImmutable? left, CircleImmutable? right)
+        {
+            return !(left == right);
+        }
+        #endregion
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Radius, Radius);
+        }
     }
 }
